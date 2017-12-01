c868bbd2fe679e87a6c151bdfbf178b4dd1cdfc9
Ana Paula Gomes <apgomes88@gmail.com>
2017-12-01 23:39:28 +0100

    Adopt pytest fixture to mock and adapt to new design

--- tests/test_gender_api.py
+++ tests/test_gender_api.py
@@ -1,68 +1,61 @@
-from detector.genderapi import GenderDetector
-from detector.genderapi import EmptyName
+from detector import gender
+from detector.gender import EmptyName
 import mock
 import pytest
 
 
-@mock.patch('detector.genderapi.requests.get')
-def test_should_return_female_when_the_first_name_is_from_female(mock_gender_api):
+@pytest.fixture
+def mock_requests(mocker):
+    return mocker.patch('detector.gender.requests.get')
+
+
+def test_should_return_female_when_the_first_name_is_from_female(mock_requests):
     expected_result = {'name':'Ana', 'gender':'female', 'probability':1, 'count':23}
-    mock_gender_api.return_value.ok = True
-    mock_gender_api.return_value.json.return_value = expected_result
+    mock_requests.return_value.ok = True
+    mock_requests.return_value.json.return_value = expected_result
 
-    detector = GenderDetector()
-    result = detector.run('Ana Ferreira')
+    result = gender.find_by('Ana Ferreira')
 
-    mock_gender_api.assert_called_once()
+    mock_requests.assert_called_once()
     assert result['gender'] == 'female'
 
 
-@mock.patch('detector.genderapi.requests.get')
-def test_should_return_male_when_the_first_name_is_from_male(mock_gender_api):
+def test_should_return_male_when_the_first_name_is_from_male(mock_requests):
     expected_result = {'name':'Mateus', 'gender':'male', 'probability':1, 'count':23}
-    mock_gender_api.return_value.ok = True
-    mock_gender_api.return_value.json.return_value = expected_result
+    mock_requests.return_value.ok = True
+    mock_requests.return_value.json.return_value = expected_result
 
-    detector = GenderDetector()
-    result = detector.run('Mateus Costa')
+    result = gender.find_by('Mateus Costa')
 
-    mock_gender_api.assert_called_once()
+    mock_requests.assert_called_once()
     assert result['gender'] == 'male'
 
 
-@mock.patch('detector.genderapi.requests.get')
-def test_should_return_unidentified_when_the_api_can_not_find_the_gender(mock_gender_api):
+def test_should_return_unidentified_when_the_api_can_not_find_the_gender(mock_requests):
     expected_result = {'name':'Usineide', 'gender': None}
-    mock_gender_api.return_value.ok = True
-    mock_gender_api.return_value.json.return_value = expected_result
+    mock_requests.return_value.ok = True
+    mock_requests.return_value.json.return_value = expected_result
 
-    detector = GenderDetector()
-    result = detector.run('Usineide Ferreira')
+    result = gender.find_by('Usineide Ferreira')
 
     assert result['gender'] == 'unidentified'
 
 
-@mock.patch('detector.genderapi.requests.get')
-def test_should_get_just_first_name(mock_gender_api):
+def test_should_get_just_first_name(mock_requests):
     expected_result = {'name':'Mateus', 'gender':'male', 'probability':1, 'count':23}
-    mock_gender_api.return_value.ok = True
-    mock_gender_api.return_value.json.return_value = expected_result
+    mock_requests.return_value.ok = True
+    mock_requests.return_value.json.return_value = expected_result
 
-    detector = GenderDetector()
-    result = detector.run('Mateus Costa')
+    result = gender.find_by('Mateus Costa')
 
     assert result['first_name'] == 'Mateus'
 
 
 def test_should_throw_an_exception_when_the_name_is_none():
-    detector = GenderDetector()
-
     with pytest.raises(EmptyName):
-        detector.run(None)
+        gender.find_by(None)
 
 
 def test_should_throw_an_exception_when_the_name_is_empty():
-    detector = GenderDetector()
-
-    with pytest.raises(EmptyName):#Exception eh muito generica!
-        detector.run('')
+    with pytest.raises(EmptyName):  # apenas Exception eh generico demais!
+        gender.find_by('')
