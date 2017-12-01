f4bda912c462b10e5d4c0ffdb67d77d582afc2e4
Ana Paula Gomes <apgomes88@gmail.com>
2017-12-01 23:39:54 +0100

    Improve names and use functions instead of class

---
+++ detector/gender.py
@@ -0,0 +1,25 @@
+import requests
+
+
+class EmptyName(Exception):
+    pass
+
+
+def find_by(name):
+    if name == '' or name is None:
+        raise EmptyName('The name is empty!')
+
+    first_name = _extract_first_name(name)
+    result = _search_on_api(first_name)
+
+    data = {
+        'first_name': first_name,
+        'gender': result['gender'] if result['gender'] else 'unidentified'
+    }
+    return data
+
+def _search_on_api(name):
+    return requests.get('https://api.genderize.io/?name={}'.format(name)).json()
+
+def _extract_first_name(name):
+    return name.split(' ')[0]
